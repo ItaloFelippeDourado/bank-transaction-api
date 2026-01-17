a2bfd334bac51dbec63dfe2a5e81881e0317ab7d
Italo Dourado <italo.dourado_v8tech@prestador.globo>
2026-01-17 17:26:45 -0300

    H2 database and swagger included

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Bank Transaction API
 
 This is a Spring Boot application for a bank transaction API.
+
+## Database
+
+The application uses a H2 database to store transactions:
+http://localhost:8080/bank-transaction/h2-console/
+
+## API
+
+The API is available at http://localhost:8080/bank-transaction/swagger-ui/index.html
--- src/main/java/com/natixis/bank_transaction_api/application/controllers/TransactionController.java
+++ src/main/java/com/natixis/bank_transaction_api/application/controllers/TransactionController.java
@@ -1,14 +1,18 @@
 package com.natixis.bank_transaction_api.application.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/transactions")
+@RequestMapping("/transaction")
+@Tag(name = "Transaction", description = "Bank transaction operations")
 public class TransactionController {
 
     @GetMapping
+    @Operation(summary = "Get transactions", description = "Returns 'transactions'")
     public String getTransactions() {
         return "transactions";
     }
